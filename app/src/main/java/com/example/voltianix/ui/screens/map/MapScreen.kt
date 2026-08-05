f77335f72cf02546f53b5613c5867878ea9fb420
ChaosKingM <emmanuelelegantini@gmail.com>
2026-08-04 21:35:31 -0600

    fix(MapScreen): fix infinite maps displating in tiles and limited the zoom the user can make in the app

--- app/src/main/java/com/example/voltianix/ui/screens/map/MapScreen.kt
+++ app/src/main/java/com/example/voltianix/ui/screens/map/MapScreen.kt
@@ -1,4 +1,4 @@
-package com.icescream.voltianix.ui
+package com.icescream.voltianix.ui.screens.map
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -9,7 +9,10 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.BoundingBox
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 
@@ -17,16 +20,40 @@ import org.osmdroid.views.MapView
 fun MapScreen(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val mapView = remember {
+
+        Configuration.getInstance().apply {
+            userAgentValue = context.packageName
+
+            cacheMapTileCount = 36
+            cacheMapTileOvershoot = 36
+
+            tileDownloadThreads = 4
+        }
+
         MapView(context).apply {
             setTileSource(TileSourceFactory.MAPNIK)
             setMultiTouchControls(true)
+            isHorizontalMapRepetitionEnabled = true
+            isVerticalMapRepetitionEnabled = false
+            minZoomLevel = 4.0
+
+            val verticalBounds = BoundingBox(
+                85.0,
+                180000.0,
+                -85.0,
+                -180000.0
+            )
+
+            setScrollableAreaLimitDouble(verticalBounds)
+
+            setExpectedCenter(GeoPoint(21.876, -102.296))
+
             controller.setZoom(15.0)
-            controller.setCenter(GeoPoint(40.416775, -3.703790)) // Madrid como ejemplo
+            controller.setCenter(GeoPoint(21.876, -102.296))
         }
     }
 
-    // Gestionar el ciclo de vida del MapView
-    val lifecycleOwner = LocalLifecycleOwner.current
+    val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
     DisposableEffect(lifecycleOwner) {
         val observer = LifecycleEventObserver { _, event ->
             when (event) {
